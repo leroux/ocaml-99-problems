f0b2718c3caf33a37ad6a55154b47d0cbb43d796
Muhaimin Ahsan <leroux@fezrev.com>
2013-07-22 22:43:03 -0400

    Solved more problems.

--- ocaml99.ml
+++ ocaml99.ml
@@ -1,10 +1,10 @@
 let rec last = function
-  | []    -> None
-  | [x]   -> Some x
+  | [] -> None
+  | [x] -> Some x
   | _::xs -> last xs
 
 let rec last_two = function
-  | []     -> None
+  | [] -> None
   | [x; y] -> Some (x, y)
   | _::xs  -> last_two xs
 
@@ -14,14 +14,14 @@ let rec at k = function
 
 let length l =
   let rec length' n = function
-    | []    -> n
-    | _::xs -> length' (n + 1) xs in
+    | [] -> n
+    | _ :: xs -> length' (n + 1) xs in
   length' 0 l
 
 let reverse l =
   let rec reverse' acc = function
-  | []    -> acc
-  | x::xs -> reverse' (x::acc) xs in
+    | [] -> acc
+    | x :: xs -> reverse' (x :: acc) xs in
   reverse' [] l
 
 let is_palindrome l = reverse l = l
@@ -32,9 +32,29 @@ type 'a node =
 
 let flatten xs =
   let rec flatten' acc = function
-    | []           -> acc
-    | One x :: xs  -> flatten' (x :: acc) xs
+    | [] -> acc
+    | One x :: xs -> flatten' (x :: acc) xs
     | Many x :: xs -> flatten' (flatten' acc x) xs in
   reverse (flatten' [] xs)
 
+let rec compress = function
+  | x :: (y :: _ as xs) -> if x == y then compress xs else x :: compress xs
+  | rest -> rest
 
+let pack xs =
+  let rec pack' smallpack acc = function
+    | [] -> []
+    | [x] -> (x :: smallpack) :: acc
+    | x :: (y :: _ as xs) -> if x == y
+                               then pack' (x :: smallpack) acc xs
+                               else pack' [] ((x :: smallpack) :: acc) xs in
+  reverse (pack' [] [] xs)
+
+let encode xs =
+  let rec encode' i acc = function
+    | [] -> []
+    | [x] -> (i + 1, x) :: acc
+    | x :: (y :: ys as xs) -> if x == y
+                                then encode' (i + 1) acc xs
+                                else encode' 0 ((i + 1, x) :: acc) xs in
+  reverse (encode' 0 [] xs)
